3c8812316ac38e2ffa2d6a3fec808d437b96472c
Christopher Lynde <lyndechris@gmail.com>
2024-04-03 13:58:52 -0400

    nav hover underline animated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,16 +13,16 @@ export function Navbar() {
 				</div>
 			</Link>
 			<ul className="flex gap-12">
-				<li className="hover:underline hover:decoration-2 underline-offset-2 decoration-secondary">
+				<li className="relative after:h-[2px] after:bg-secondary  after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-full after:duration-300">
 					<Link href="/about">About</Link>
 				</li>
-				<li className="hover:underline hover:decoration-2 underline-offset-2 decoration-secondary">
+				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-full after:duration-300">
 					<Link href="/#portfolio">Portfolio</Link>
 				</li>
-				<li className="hover:underline hover:decoration-2 underline-offset-2 decoration-secondary">
+				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-[72%] after:duration-300">
 					<Link href="/blog">Blog</Link>
 				</li>
-				<li className="hover:underline hover:decoration-2 underline-offset-2 decoration-secondary text-secondary">
+				<li className="relative text-secondary after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-full after:duration-300">
 					<Link href="/#contact">Contact</Link>
 				</li>
 			</ul>
