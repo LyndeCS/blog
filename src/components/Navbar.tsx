9bba1a57944f031ed84cd68cd7a5cf68834cc4ee
Christopher Lynde <lyndechris@gmail.com>
2024-04-03 20:15:50 -0400

    nav animation tweaks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,23 +6,23 @@ export function Navbar() {
 			<Link href="/">
 				<div>
 					<span className="text-secondary">{"<"}</span>
-					<span className="hover:underline decoration-secondary underline-offset-2 decoration-2">
+					<span className="relative before:h-[2px] after:h-[2px] before:bg-secondary after:bg-secondary before:absolute after:absolute before:left-0 after:left-[58px] before:bottom-[0px] after:bottom-[0px] before:w-0 after:w-0 hover:before:w-[49px] hover:after:w-[31px] before:duration-200 before:delay-200 before:hover:delay-0 after:duration-200 after:hover:delay-200 after:delay-0">
 						Chris Lynde
 					</span>
 					<span className="text-secondary">{" />"}</span>
 				</div>
 			</Link>
 			<ul className="flex gap-12">
-				<li className="relative after:h-[2px] after:bg-secondary  after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-full after:duration-300">
+				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[2px] after:w-0 hover:after:w-full after:duration-[400ms]">
 					<Link href="/about">About</Link>
 				</li>
-				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-full after:duration-300">
+				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[2px] after:w-0 hover:after:w-full after:duration-[400ms]">
 					<Link href="/#portfolio">Portfolio</Link>
 				</li>
-				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-[72%] after:duration-300">
+				<li className="relative after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[2px] after:w-0 hover:after:w-[71%] after:duration-[400ms]">
 					<Link href="/blog">Blog</Link>
 				</li>
-				<li className="relative text-secondary after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[1px] after:w-0 hover:after:w-full after:duration-300">
+				<li className="relative text-secondary after:h-[2px] after:bg-secondary after:absolute after:left-0 after:bottom-[2px] after:w-0 hover:after:w-full after:duration-[400ms]">
 					<Link href="/#contact">Contact</Link>
 				</li>
 			</ul>
